47cf82d05cd6625a27c3e7c52d1ffed1e28b2c44
MAZE <remvze@proton.me>
2024-09-30 12:33:02 +0330

    feat: add websites

---
+++ src/data/websites.ts
@@ -0,0 +1,28 @@
+/**
+ * From Old to New
+ */
+export const websites: Array<{
+  address: string;
+  date: string;
+  name: string;
+  url: string;
+}> = [
+  {
+    address: 'nazhamid.com',
+    date: '2024-09-30',
+    name: 'Naz Hamid',
+    url: 'https://nazhamid.com/',
+  },
+  {
+    address: '2dots.xyz',
+    date: '2024-09-30',
+    name: '2dots',
+    url: 'https://www.2dots.xyz/',
+  },
+  {
+    address: 'tiagorangel.com',
+    date: '2024-09-30',
+    name: 'Tiago Rangel',
+    url: 'https://tiagorangel.com/',
+  },
+];
