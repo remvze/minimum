2a4edc098070652e5fcf6ebeedd5d5b5597103a5
MAZE <remvze@proton.me>
2024-11-15 22:15:22 +0330

    chore: add more websites

--- src/data/websites.ts
+++ src/data/websites.ts
@@ -3,32 +3,58 @@
  */
 export const websites: Array<{
   address: string;
+  date: string;
   name: string;
   screenshot: string;
   url: string;
 }> = [
   {
     address: 'once.com',
+    date: '2024-11-15',
     name: 'Once',
     screenshot: '/images/screenshots/once.com.webp',
     url: 'https://once.com/',
   },
   {
     address: 'tr.af',
+    date: '2024-11-15',
     name: 'Traf',
     screenshot: '/images/screenshots/tr.af.webp',
     url: 'https://tr.af/',
   },
   {
     address: '2dots.xyz',
+    date: '2024-11-15',
     name: '2dots',
     screenshot: '/images/screenshots/2dots.xyz.webp',
     url: 'https://2dots.xyz/',
   },
   {
     address: 'paco.me',
+    date: '2024-11-15',
     name: 'Paco Coursey',
     screenshot: '/images/screenshots/paco.me.webp',
     url: 'https://paco.me/',
+  },
+  {
+    address: 'reaper.is',
+    date: '2024-11-15',
+    name: 'Reaper',
+    screenshot: '/images/screenshots/reaper.is.webp',
+    url: 'https://reaper.is/',
+  },
+  {
+    address: 'hiddenj.com',
+    date: '2024-11-15',
+    name: 'J',
+    screenshot: '/images/screenshots/hiddenj.com.webp',
+    url: 'https://hiddenj.com/',
+  },
+  {
+    address: 'bmrks.com',
+    date: '2024-11-15',
+    name: '(Basic) Bookmarks',
+    screenshot: '/images/screenshots/bmrks.com.webp',
+    url: 'https://bmrks.com/',
   },
 ];
